6a4fa85b8fe5b097eb053750b0b67275fde32054
Alican Sungur <sunguralican@gmail.com>
2022-03-26 18:25:02 +0000

    Configuration
    - Add proxy for development setup
    - Add logo
    - Add colors and font to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,22 @@ module.exports = {
      "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        midnightblue: '#0B1D74',
+        midnightblue2: '#19224D',
+        bdazzledblue: '#485C87',
+        blueryb: '#3350DE',
+        cornflowerblue: '#7B9EFF',
+        redorange: '#F85523',
+        tomato: '#F46C51',
+        white: '#FFFFFF',
+        white2: '#EBEBF2'
+      },
+      fontFamily: {
+        sans: ['Karla', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
